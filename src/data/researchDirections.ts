93572be0caa9edea962d8163c147ab105bd651f6
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2025-02-11 22:44:02 +0800

    更新首页图片

--- src/data/researchDirections.ts
+++ src/data/researchDirections.ts
@@ -3,24 +3,24 @@ export const researchDirections = [
         id: 1,
         title: "Physical-virtual twining via collaborative XR",
         description: "It enables real-time physical-virtual interaction and collaboration through XR.",
-        image: "/img/1.webp",
+        image: "/img/1-1.webp",
     },
     {
         id: 2,
         title: "City and building informatics",
         description: "Modular construction and smart construction management.",
-        image: "/img/2.webp",
+        image: "/img/2-1.webp",
     },
     {
         id: 3,
         title: "Sensing and vision capturing in built environments",
         description: "Virtual prototyping and information visualization using collaborative AR/VR.",
-        image: "/img/3.webp",
+        image: "/img/3-1.webp",
     },
     {
         id: 4,
         title: "Human behavior interpretation",
         description: "Indoor navigation with multi-sensor data fusion.",
-        image: "/img/4.webp",
+        image: "/img/4-1.webp",
     },
 ];
